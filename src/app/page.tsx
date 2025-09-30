a49d24e9725c2c12612a831e67a72e70f1f15dea
dongwonAhn <ado2188@naver.com>
2025-09-30 16:48:41 +0900

    feat. 랜딩 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Button from '@/components/ui/button';
+import Input from '@/components/ui/input';
 
 export default function Home() {
   return (
     <div>
       <Button>로그인</Button>
+      <Input
+        id="email"
+        name="email"
+        label="이메일"
+        placeholder="이메일을 입력해주세요."
+      />
     </div>
   );
 }
