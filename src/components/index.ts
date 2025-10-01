4824c77c50c5ce0c69bac9204fefd89f560eb298
dongwonAhn <ado2188@naver.com>
2025-10-01 09:24:10 +0900

    refactor. index.ts에 배럴 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
+export { default as Button } from './ui/Button';
+export { default as Input } from './ui/Input';
+export { default as Modal } from './ui/Modal';
 export { default as Notification } from './ui/Notification';
 export { default as Section } from './ui/Section';
