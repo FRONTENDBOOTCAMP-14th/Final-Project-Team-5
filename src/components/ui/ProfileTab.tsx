11deb5d43d59394f45cdcf0552ebf2d66221be83
DESKTOP-867TAIF\gomte <gomteang@nate.com>
2025-10-10 10:39:52 +0900

    feat. profiletab 기능 구현

---
+++ src/components/ui/ProfileTab.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState } from 'react';
+// import { codiList } from '../../libs/supabase/Codilist';
+import FavoriteCodiList from './FavoriteCodiList';
+import MyCodiList from './MyCodiList';
+
+export default function ProfileTab() {
+  const [activeTab, setActiveTab] = useState<'favorite' | 'my'>('favorite');
+
+  return (
+    <div className="w-full max-w-md mx-auto p-4">
+      <div className="flex">
+        <button
+          onClick={() => setActiveTab('favorite')}
+          className={`w-1/2 pb-2 text-center font-mideum transition-colors ${
+            activeTab === 'favorite'
+              ? 'text-blue-500 font-semibold'
+              : 'text-gray-500'
+          }`}
+        >
+          관심 코디
+        </button>
+        <button
+          onClick={() => setActiveTab('my')}
+          className={`w-1/2 pb-2 text-center font-mideum transition-colors ${
+            activeTab === 'my' ? 'text-blue-500 font-semibold' : 'text-gray-500'
+          }`}
+        >
+          나의 코디
+        </button>
+      </div>
+      <div>
+        {activeTab === 'favorite' ? (
+          <FavoriteCodiList codiList={codiList} />
+        ) : (
+          <MyCodiList codiList={codiList} />
+        )}
+      </div>
+    </div>
+  );
+}
