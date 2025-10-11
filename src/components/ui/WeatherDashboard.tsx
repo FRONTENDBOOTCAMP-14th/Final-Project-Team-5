a2a45a00dc0a14651a02359443637c88937f3dd4
MINJI <always0524@naver.com>
2025-10-11 21:24:54 +0900

    feat. 날씨 대시보드 API 도시명, 현재 온도 구현

--- src/components/ui/WeatherDashboard.tsx
+++ src/components/ui/WeatherDashboard.tsx
@@ -1,6 +1,12 @@
+'use client';
+
 import { ChevronRight, MapPin } from 'lucide-react';
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 const weatherProps = {
   src: '/weather/sunny.svg',
@@ -19,6 +25,87 @@ interface ImageProps {
 }
 
 export default function WeatherDashboard(props: ImageProps) {
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
+  // const [location, setLocation] = useState(null);
+  const [locdata, setLocdata] = useState(null);
+  const [data, setData] = useState(null);
+
+  // 현재 위치 가져오기
+  useEffect(() => {
+    function OnGeoOk(position) {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    }
+
+    console.log('첫번째', lat, lon);
+
+    function OnGeoError() {
+      throw new Error('Can not find you!');
+    }
+
+    navigator.geolocation.getCurrentPosition(OnGeoOk, OnGeoError);
+  }, []);
+
+  useEffect(() => {
+    // 현재 위치 기반 날씨 가져오기
+    async function FetchWeatherData() {
+      const URL = `${BASE_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+      if (!URL) {
+        throw new Error('API URL is not defined!');
+      }
+
+      if (!API_KEY) {
+        throw new Error('API KEY is not defined!');
+      }
+
+      try {
+        const response = await fetch(URL);
+        if (!response.ok) throw new Error('날씨 데이터를 가져오지 못했습니다!');
+        const responseData = await response.json();
+        setData(responseData);
+        console.log(responseData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (lat && lon) {
+      FetchWeatherData();
+    }
+  }, [lat, lon]);
+
+  console.log('두번째', data?.main?.temp);
+
+  const locationTemp = data?.main.temp || '현재 온도';
+
+  // 현재 위치명 한국어로 변경
+  useEffect(() => {
+    async function TranslateLocation() {
+      const TransURL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=6&appid=${API_KEY}`;
+
+      try {
+        const ress = await fetch(TransURL);
+        if (!ress.ok) throw new Error('반대 데이터를 가져오지 못했습니다!');
+        const ressData = await ress.json();
+        setLocdata(ressData);
+        console.log(ressData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    TranslateLocation();
+  }, [lat, lon]);
+
+  useEffect(() => {
+    if (!locdata) return;
+    console.log('세번째', locdata?.[0]?.local_names?.ko);
+  }, [locdata]);
+
+  const locationName = locdata?.[0]?.local_names?.ko || '현재 위치';
+
   return (
     <div className="w-[340px] h-[228px] p-3 rounded-2xl mx-auto bg-[#FFFFFF]">
       {/* 위치 및 더보기 버튼 */}
@@ -26,7 +113,7 @@ export default function WeatherDashboard(props: ImageProps) {
         {/* 현재위치 클릭시 내 위치선택 모달창 연결 */}
         <button type="button" className="flex gap-[1px] cursor-pointer">
           <MapPin />
-          {props.location}
+          {locationName}
         </button>
         {/* 더보기 클릭시 현재위치 날씨 및 일주일 기상정보 페이지로 이동 */}
         <Link href="/" className="flex">
@@ -36,7 +123,7 @@ export default function WeatherDashboard(props: ImageProps) {
       </div>
       {/* 현재 온도 및 간략한 날씨정보란 */}
       <div className="flex place-content-between my-7">
-        <div className="text-5xl font-bold">25°C</div>
+        <div className="text-5xl font-bold">{locationTemp}°C</div>
         <div>
           <p className="flex">
             <Image
