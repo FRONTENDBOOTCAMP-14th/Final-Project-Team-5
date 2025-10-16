2d85bb91cb01e24dfd0d6dc3ca43f377c3d0167c
MINJI <always0524@naver.com>
2025-10-16 17:05:17 +0900

    feat. 전역 타입변수 파일 분리

---
+++ src/@types/global.d.ts
@@ -0,0 +1,36 @@
+export interface WeatherData {
+  dt: number;
+  dt_txt: string;
+  main: {
+    temp: number | undefined;
+    temp_min: number | undefined;
+    temp_max: number | undefined;
+  };
+  weather: {
+    main: string;
+    description: string;
+  }[];
+}
+
+export interface LocationData {
+  results: {
+    region: {
+      area1?: { name: string };
+      area2?: { name: string };
+      area3?: { name: string };
+    };
+  }[];
+}
+
+export interface TempData {
+  list: {
+    dt: number | undefined;
+    main: {
+      temp: number;
+    };
+    weather: {
+      main: string;
+      description: string;
+    }[];
+  }[];
+}
