87a6e65090ea0278e7bad90c3778a457bfbaa1d4
dongwonAhn <ado2188@naver.com>
2025-10-16 13:21:52 +0900

    feat. 회원가입 환영 모달 테스트 페이지

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,16 +2,14 @@
 
 'use client';
 
-import Button from '@/components/ui/Button';
 import Frame from '@/components/ui/Frame';
 import ImageForm from '@/components/ui/ImageForm';
 import ImageList from '@/components/ui/ImageList';
-import Input from '@/components/ui/Input';
 import Modal from '@/components/ui/Modal';
-import SnsButton from '@/components/ui/SnsButton';
 import Spinner from '@/components/ui/Spinner';
 
 import Notification from '@/components/ui/Notification';
+import OnboardingModal from '@/components/ui/OnboardingModal';
 import WeatherDashboard from '@/components/ui/WeatherDashboard';
 import { useState } from 'react';
 import { Toaster } from 'sonner';
@@ -19,16 +17,11 @@ import { Toaster } from 'sonner';
 export default function Home() {
   // 알림창 다이얼로그 기능 테스트용 상태
   const [openDialog, setOpenDialog] = useState(false);
+  // 웰컴 모달 테스트
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <Frame>
-      <Button>로그인</Button>
-      <Input
-        id="email"
-        name="email"
-        label="이메일"
-        placeholder="이메일을 입력해주세요."
-      />
       <Modal></Modal>
       {/* 알림창 오픈확인을 위한 임시버튼 */}
       <button
@@ -49,13 +42,7 @@ export default function Home() {
       <ImageForm />
       <Spinner size="sm" className="py-2"></Spinner>
       <ImageList src1="/" src2="/" />
-      <div className="flex gap-4 justify-center p-8">
-        <SnsButton
-          provider="kakao"
-          onClick={() => console.log('카카오 클릭')}
-        />
-        <SnsButton provider="google" onClick={() => console.log('구글 클릭')} />
-      </div>
+
       <WeatherDashboard
         src="/weather/sunny.svg"
         width={22}
@@ -63,6 +50,7 @@ export default function Home() {
         location="강남구 역삼동"
         condition="맑음"
       />
+      <OnboardingModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
     </Frame>
   );
 }
