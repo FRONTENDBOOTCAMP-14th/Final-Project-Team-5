6f271834ab43bfe6e8f7730455504d6a68823089
DESKTOP-867TAIF\gomte <gomteang@nate.com>
2025-10-17 10:15:55 +0900

    fix. profilepage 기능개선

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,18 @@
 'use client';
 
 // import CodiList from '../../components/ui/CodiList';
+import NavigationBar from '../../components/ui/NavigationBar';
 import ProfileSheet from '../../components/ui/ProfileSheet';
+import ProfileTab from '../../components/ui/ProfileTab';
 // import { codiList } from '../../libs/supabase/Codilist';
 
 export default function ProfilePage() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
-      <div>
-        <ProfileSheet />
-        {/*<ProfileTab codiList={codiList} />*/}
-        {/* <NavBar /> */}
-      </div>
+      <ProfileSheet />
+      <ProfileTab />
+      {/* <CodiList /> */}
+      <NavigationBar />
     </div>
   );
 }
