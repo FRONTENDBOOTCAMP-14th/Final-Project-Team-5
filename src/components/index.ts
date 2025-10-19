bd5d96f9a57298c34cafc81df90e4ec9e39f1795
MINJI <always0524@naver.com>
2025-10-19 21:45:43 +0900

    refactor. 현재 위치 날씨 정보 컴포넌트 분리구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,3 +13,4 @@ export { default as SnsButton } from './ui/SnsButton';
 export { default as Spinner } from './ui/Spinner';
 export { default as WeatherDashboard } from './ui/WeatherDashboard';
 export { default as WeatherWeekly } from './ui/WeatherWeekly';
+export { default as LocationTemp } from './ui/LocationTemp';
