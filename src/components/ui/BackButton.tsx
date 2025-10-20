ffcec25496fe76919bf80bac34698cd29c3ec74e
IceJack <twwj4134@gmail.com>
2025-10-21 02:11:38 +0900

    feat. 백버튼 기능 추가

--- src/components/ui/BackButton.tsx
+++ src/components/ui/BackButton.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import React from 'react';
+import { useRouter } from 'next/navigation';
 import { ArrowLeft } from 'lucide-react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -7,13 +10,24 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 const BackButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className = '', children, ...props }, ref) => {
+    const router = useRouter();
+
     const containerStyles =
       'w-full px-4 py-3 text-black font-bold rounded-lg flex items-center gap-2';
     const iconButtonStyles = 'p-2 rounded-lg hover:bg-gray-100 transition';
 
     return (
       <div className={`${containerStyles} ${className}`}>
-        <button ref={ref} className={iconButtonStyles} {...props}>
+        <button
+          ref={ref}
+          className={iconButtonStyles}
+          onClick={(e) => {
+            props.onClick?.(e);
+            if (e.defaultPrevented) return;
+            router.back();
+          }}
+          {...props}
+        >
           <ArrowLeft size={44} className="text-[#000]" />
         </button>
         {children && <span className="select-none">{children}</span>}
