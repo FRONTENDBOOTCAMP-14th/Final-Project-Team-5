f82ad02f307b2a3c8f33abc08d8930bf4cff1f3c
MINJI <always0524@naver.com>
2025-10-21 17:48:50 +0900

    feat. 날씨 대시보드 위치 검색 및 변경위치 주소 렌더링 구현

--- src/components/ui/WeatherDashboard.tsx
+++ src/components/ui/WeatherDashboard.tsx
@@ -1,44 +1,31 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { ChevronRight, MapPin } from 'lucide-react';
+import { ChevronRight, MapPin, X, Search } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { toast } from 'sonner';
-import type { LocationData } from '@/@types/global.d.ts';
+import type { Document } from '@/@types/global';
 import useGeoLocation from '@/hooks/useGeoLocation';
-import GetLocation from '@/libs/getLocation';
+import useLocationData from '@/hooks/useLocationData';
+import LoadSearch from '@/libs/loadSearch';
+import Button from './Button';
+import Input from './Input';
 import LocationTemp from './LocationTemp';
+import Modal from './Modal';
 
 export default function WeatherDashboard() {
-  const [locationData, setLocationData] = useState<LocationData | null>(null);
   const [locationName, setLocationName] = useState<string | undefined>('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState<Document | null>(null);
+  const [isSelect, setIsSelect] = useState<number | null>(null);
 
   // 현재 위치 가져오기
   const { lat, lon } = useGeoLocation();
 
-  useEffect(() => {
-    if (!lat || !lon) return;
-
-    const abortController = new AbortController();
-
-    // 현재 위치명 한국어로 변경
-    async function GetLocationData() {
-      try {
-        const location = await GetLocation(lat, lon);
-        setLocationData(location);
-      } catch (error) {
-        console.error(error);
-        toast.error('현재 위치를 한국어로 불러오지 못했습니다!');
-      }
-    }
-
-    void GetLocationData();
-
-    return () => {
-      abortController.abort();
-    };
-  }, [lat, lon]);
+  // 위치 변경시 주소 변경되게하기
+  const { locationData, GetLocationData } = useLocationData(lat, lon);
 
   useEffect(() => {
     if (!locationData) return;
@@ -53,15 +40,142 @@ export default function WeatherDashboard() {
 
   const pathName = usePathname();
 
+  // 창 닫기
+  function handleClose() {
+    setIsOpen(false);
+  }
+
+  // 주소 검색하기
+  async function handleSearch() {
+    if (!query) return;
+    try {
+      const searchTerm = (await LoadSearch(query)) as Document;
+      setResults(searchTerm);
+    } catch (error) {
+      console.log(error);
+      toast.error('주소 검색창을 불러오지 못했습니다!');
+    }
+  }
+
   return (
     <div className="h-[228px] p-3 rounded-2xl ml-11 mr-11 mx-auto bg-[#FFFFFF]">
       {/* 위치 및 더보기 버튼 */}
       <div className="flex place-content-between">
         {/* 현재위치 클릭시 내 위치선택 모달창 연결 */}
-        <button type="button" className="flex gap-[1px] cursor-pointer">
+        <button
+          type="button"
+          className="flex gap-[1px] cursor-pointer"
+          onClick={() => setIsOpen(true)}
+        >
           <MapPin />
           {locationName}
         </button>
+        {/* 모달창 */}
+        {isOpen && (
+          <Modal
+            onClose={() => {
+              setIsOpen(false);
+              setQuery('');
+              setResults(null);
+            }}
+          >
+            <div className="relative">
+              <Input
+                id="address"
+                label=""
+                value={query}
+                placeholder="예) 도로명 주소로 검색해보세요 (예: 반포대로 58)"
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') void handleSearch();
+                }}
+                className="mb-4"
+              />
+              <button
+                type="reset"
+                className="absolute cursor-pointer right-10 top-4"
+                onClick={() => {
+                  setQuery('');
+                  setResults(null);
+                }}
+              >
+                <X size={16} aria-label="검색어 삭제" />
+              </button>
+              <button
+                type="button"
+                className="absolute cursor-pointer right-2 top-4"
+                onClick={() => void handleSearch()}
+              >
+                <Search aria-label="검색하기" />
+              </button>
+            </div>
+
+            {/* 결과표시 */}
+            <table className="w-full ">
+              <tbody>
+                <tr className="gap-6">
+                  <th className="text-left py-2">우편번호</th>
+                  <th className="text-left py-2">도로명 주소</th>
+                  <th className="text-left py-2">선택여부</th>
+                </tr>
+                {results?.documents?.map((doc, index) => (
+                  <tr
+                    key={index}
+                    tabIndex={0}
+                    className={`h-[44px] px-1 ${
+                      results?.documents?.length
+                        ? 'hover:bg-[#EBF3FE] cursor-pointer'
+                        : ''
+                    }`}
+                  >
+                    <td className="py-2">
+                      {results?.documents?.[0]?.road_address?.zone_no && (
+                        <>({results.documents[0].road_address.zone_no})</>
+                      )}
+                    </td>
+                    <td className="py-2">
+                      {results?.documents?.[0]?.address_name}
+                      {results?.documents?.[0]?.road_address?.building_name && (
+                        <>({results.documents[0].road_address.building_name})</>
+                      )}
+                    </td>
+                    <td className="text-center items-center">
+                      <input
+                        type="checkbox"
+                        className="border-[1px] w-5 h-5"
+                        checked={isSelect === index}
+                        onChange={() =>
+                          setIsSelect(isSelect === index ? null : index)
+                        }
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            <Button
+              type="button"
+              className="mb-auto mt-4 cursor-pointer"
+              onClick={() => {
+                if (isSelect !== null) {
+                  const selected = results?.documents[isSelect];
+                  const addressLat = Number(selected?.y);
+                  const addressLon = Number(selected?.x);
+
+                  void GetLocationData(addressLat, addressLon);
+                  handleClose();
+                } else {
+                  toast('주소를 선택해주세요!');
+                }
+                setQuery('');
+                setResults(null);
+              }}
+            >
+              변경
+            </Button>
+          </Modal>
+        )}
         {/* 더보기 클릭시 현재위치 날씨 및 일주일 기상정보 페이지로 이동 */}
         {pathName !== '/weather' && (
           <Link href="/weather" className="flex">
