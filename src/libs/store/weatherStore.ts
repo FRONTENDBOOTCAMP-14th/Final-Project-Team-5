125623c9343741d5c4954e1eb27544860d9e083d
MINJI <always0524@naver.com>
2025-10-25 22:15:02 +0900

    feat. 위치에 따른 날씨 정보 상태관리를 위한 zustand store 구현

--- src/libs/store/weatherStore.ts
+++ src/libs/store/weatherStore.ts
@@ -3,14 +3,22 @@ import { create } from 'zustand';
 export const useWeatherStore = create<{
   currentLat: number | undefined;
   currentLon: number | undefined;
+  selectedLocation: string | undefined;
   setLocation: (
     currentLat: number | undefined,
-    currentLon: number | undefined
+    currentLon: number | undefined,
+    selectedLocation: string | undefined
   ) => void;
 }>((set) => {
   return {
     currentLat: undefined,
     currentLon: undefined,
-    setLocation: (lat, lon) => set({ currentLat: lat, currentLon: lon }),
+    selectedLocation: undefined,
+    setLocation: (lat, lon, address) =>
+      set({
+        currentLat: lat,
+        currentLon: lon,
+        selectedLocation: address,
+      }),
   };
 });
